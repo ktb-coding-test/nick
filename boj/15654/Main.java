d1efeda2e7c2e3823bdbd0f67d70c83e18be1bee
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-16 11:28:28 +0900

    [BOJ] 15657번

--- boj/15654/Main.java
+++ boj/15654/Main.java
@@ -4,12 +4,11 @@ import java.io.*;
 public class Main {
     
     static int[] arr;
-    static boolean[] visited;
     static int N;
     static int M;
     static StringBuilder sb = new StringBuilder();
     
-    public static void recursion(String str, int cnt) {
+    public static void recursion(int startIdx, String str, int cnt) {
         
         if (cnt == M) {
             sb.append(str.trim() + "\n");
@@ -17,12 +16,8 @@ public class Main {
         }
         
         
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                recursion(str + arr[i] + " ", cnt + 1);
-                visited[i] = false;
-            }
+        for (int i = startIdx; i < N; i++) {
+            recursion(i, str + arr[i] + " ", cnt + 1);
         }
     }
     
@@ -36,7 +31,6 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
       
         arr = new int[N];
-        visited = new boolean[N];
       
         st = new StringTokenizer(br.readLine());
       
@@ -46,7 +40,9 @@ public class Main {
         
         Arrays.sort(arr);
       
-        recursion("", 0);
+        for (int i = 0; i < N; i++) {
+            recursion(i, arr[i] + " ", 1);
+        }
         
         bw.write(sb + "\n");
         bw.close();
